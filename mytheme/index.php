618c58b77c7aa4dc11731a2106bbcdc7b4bb6abd
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-06-10 20:27:15 +0500

    add prior wordpress custom theme code

--- mytheme/index.php
+++ mytheme/index.php
@@ -2,14 +2,21 @@
 
 
 <body>
-    <div>
-        <h1>This is Theme Development Course</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sed architecto cumque quidem alias, id accusamus sequi officia iste tempore?</p>
-    </div>
-    <div>
-        <h1>This is Theme Development Course</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sed architecto cumque quidem alias, id accusamus sequi officia iste tempore?</p>
-    </div>
+  <?php 
+     $query = new WP_Query(array(
+        'category_name' => 'Books',
+     ));
+
+    if($query->have_posts()): $query -> the_post();
+        while($query->have_posts()): 
+            //code to display posts goes here
+           
+            $query -> the_title();
+        endwhile;
+    endif;
+    //Reset post data to avoid conflicts
+    wp_reset_postdata();
+  ?>
 </body>
 
 <?php get_footer(); ?>
