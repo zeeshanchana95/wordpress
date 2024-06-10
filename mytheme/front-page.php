1cd147ad82e331bf3c957f9f0b6cc996fa663104
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-06-10 20:36:19 +0500

    showing posts

---
+++ mytheme/front-page.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+
+<h1>This is front page</h1>
+
+<?php get_footer();?>
--- mytheme/index.php
+++ mytheme/index.php
@@ -3,15 +3,10 @@
 
 <body>
   <?php 
-     $query = new WP_Query(array(
-        'category_name' => 'Books',
-     ));
-
-    if($query->have_posts()): $query -> the_post();
-        while($query->have_posts()): 
+    if(have_posts()):
+        while(have_posts()): the_post();
             //code to display posts goes here
-           
-            $query -> the_title();
+            the_title();
         endwhile;
     endif;
     //Reset post data to avoid conflicts
