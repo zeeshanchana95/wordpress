333ea3dbba218053ce6ce77151277df6b6a620c6
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-06-18 15:33:24 +0500

    add login form using bootstrap in wordpress theme

---
+++ mytheme2/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .width {
+            width: 150px;
+        }
+    </style>
+</head>
+  <body>
+    
+    <h1 style="text-align:center; color:skyblue;">Login Form in WordPress</h1>
+    
+    <div class="container">
+        <form action="">
+            <div class="input-group mb-3">
+                <span class="input-group-text width" id="basic-addon1">Username: </span>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>    
+
+            <div class="input-group mb-3">
+                <span class="input-group-text width" id="basic-addon1">Email: </span>
+                <input type="text" class="form-control" placeholder="email" aria-label="email" aria-describedby="basic-addon1">
+            </div>    
+            
+            <div class="input-group mb-3">
+                <span class="input-group-text width" id="basic-addon1">Password: </span>
+                <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+            </div>    
+            
+            <div class="input-group mb-3">
+                <span class="input-group-text width" id="basic-addon1">Confirm Password: </span>
+                <input type="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1">
+            </div>    
+        </form>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
