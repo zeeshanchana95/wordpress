4b1ea0c98e74773dc3346325afe61ba371fcdb1a
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-06-19 00:18:19 +0500

    add code for favicon

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -1,6 +1,11 @@
 <?php 
+
     function my_theme_enqueue_styles() {
         wp_enqueue_style( 'my_theme_style', get_stylesheet_uri() );
+        
+        wp_enqueue_style( 'header-styles', get_template_directory_uri() . '/styles/header-styles.css' );
+        
+        wp_enqueue_script('header-js', get_template_directory_uri() . '/js/header.js');
     }
     add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
@@ -9,4 +14,13 @@
         register_nav_menu('primary', __('Primary Menu', 'my_theme'));
       }
       add_action('after_setup_theme', 'register_my_menu');
+
+
+      function add_favicon() {
+        $favicon_url = get_stylesheet_directory_uri() . '/favicon.ico';
+        echo '<link rel="shortcut icon" href="' . $favicon_url . '" />';
+    }
+    add_action('wp_head', 'add_favicon'); // Adds favicon to the front-end
+    add_action('login_head', "add_favicon");
+    add_action('admin_head', "add_favicon");
 ?>
--- mytheme/header.php
+++ mytheme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Day 07 WordPress Theme Files</title>
+    <title>Day 10 WordPress Features</title>
 </head>
 <?php 
     // to show menu on our wordpress theme [used in heade.php to show menu in header section]
